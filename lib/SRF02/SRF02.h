ef2bb91a31a925079db42ce78732e790af81f463
Monteskalier0 <ja.touboul.r@gmail.com>
2024-03-11 10:15:07 +0100

    SRF02?

---
+++ lib/SRF02/SRF02.h
@@ -0,0 +1,8 @@
+#include <Wire.h>
+#include <Arduino.h>
+
+int reading = 0;
+
+void sensorBegin(void);
+
+void sensorRead(void);
--- src/main.cpp
+++ src/main.cpp
@@ -2,45 +2,45 @@
 #include <math.h>
 #include "Encodeur.h"
 #include <Moteur.h>
+#include "SRF02.h"
 
-//All declaration shall be made in camelCase
-int test = 0;
+Moteur moteurDroit(20000, // frequence
+                   0,     // channel de la PWM
+                   14,    // pin de la PWM
+                   8,     // resolution
+                   12,    // pin de rotation
+                   13,    // mode
+                   17,    // encodeur pin A
+                   19     // encodeur pin B
+);
 
-Moteur moteurDroit(20000,//frequence
-                   0,//channel de la PWM
-                   14,//pin de la PWM
-                   8,//resolution
-                   12,//pin de rotation
-                   13,//mode
-                   17,//encodeur pin A 
-                   19//encodeur pin B
-                   );
+Moteur moteurGauche(20000, // frequence
+                    1,     // channel de la PWM
+                    26,    // pin de la PWM
+                    8,     // resolution
+                    27,    // pin de rotation
+                    13,    // mode
+                    33,    // encodeur pin A
+                    25     // encodeur pin B
+);
 
-Moteur moteurGauche(20000,//frequence
-                    1,//channel de la PWM
-                    26,//pin de la PWM
-                    8,//resolution
-                    27,//pin de rotation
-                    13,//mode
-                    33,//encodeur pin A 
-                    25//encodeur pin B
-                    );
-                    
 robot pami(moteurDroit, moteurGauche);
 
 Encoder encodeurDroit(17, 19);
 Encoder encodeurGauche(33, 25);
 int valD, valG;
 
-
 void setup()
 {
   Serial.begin(115200);
+  sensorBegin();
 }
 
 void loop()
 {
+
   valD = encodeurDroit.getCount();
   valG = encodeurGauche.getCount();
-   Serial.printf("Valeur codeur Gauche %d  Valeur Codeur Droit %d\n", valG, valD);
+  Serial.printf("Valeur codeur Gauche %d  Valeur Codeur Droit %d\n", valG, valD);
+
 }
